896b3e393237825f0a4306a1ae0c02e208427b9c
fengxinbao <fengxinbao@360.cn>
2022-08-11 23:00:54 +0800

    Add music bars
    Former-commit-id: d7dc404722a25eed080803f37670b8d640e0039f [formerly fa44d926c1156152494538de074e89a53a3b7e11]
    Former-commit-id: 57ea893e270e1dc91732b559f32e2353655a19d4

--- js/post-song.js
+++ js/post-song.js
@@ -1,26 +1,38 @@
 $(document).ready(function() {
   'use strict';
 
-  console.log('fxb fxb');
-  console.log(document.title);
+  const player = document.getElementById('player');
+  const audios = document.getElementsByClassName('audio');
 
-  const player = document.getElementById('song-player');
-  const songTitles = document.getElementsByClassName('song-title');
-
-  for(const songTitle of songTitles) {
-    songTitle.addEventListener('click', (e) => {
+  for(const audio of audios) {
+    audio.addEventListener('click', (e) => {
       e.preventDefault();
-      $("audio").each(function(){
-        if(!$(this)[0].paused){
-            $(this)[0].pause();
-        }
-      });
+      // $("audio").each(function(){
+        // if(!$(this)[0].paused){
+        //     $(this)[0].pause();
+        // }
+      // });
       var elm = e.target;
+console.log("fxb")
+
+      var musicBars = document.getElementsByClassName("bars");
+      Array.from(musicBars).forEach(element => {
+        element.classList.remove("now");
+      });
+      elm.classList.add("now");
+
       var source = document.getElementById('audioSource');
       source.src = "https://typora-1259024198.cos.ap-beijing.myqcloud.com/" + elm.getAttribute('data-value');
       player.load(); //call this to just preload the audio without playing
       player.play();
     });
-
   }
+
+  const loopAll = document.getElementById('loop-all');
+  const songs = document.getElementsByClassName('song');
+  loopAll.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+  });
+
 });
