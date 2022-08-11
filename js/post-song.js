b13ac3c7278d25dd634864f13c47b4e4cf0485e8
fengxinbao <fengxinbao@360.cn>
2022-08-12 00:02:32 +0800

    Add music : loop all
    Former-commit-id: d17f44565032f4b0cd6c1e1725eaf857eaab3367 [formerly 1a024da70d81e0397e8f9932b03774ea5eb2a965]
    Former-commit-id: 2a6b6da6775afa09b69b629b8fc6495b1b312194

--- js/post-song.js
+++ js/post-song.js
@@ -4,35 +4,63 @@ $(document).ready(function() {
   const player = document.getElementById('player');
   const audios = document.getElementsByClassName('audio');
 
+  // 点击歌曲名称时切换音乐
   for(const audio of audios) {
     audio.addEventListener('click', (e) => {
       e.preventDefault();
-      // $("audio").each(function(){
-        // if(!$(this)[0].paused){
-        //     $(this)[0].pause();
-        // }
-      // });
+      player.removeEventListener('ended',playEndedHandler,false);
+      player.loop = true;
       var elm = e.target;
-console.log("fxb")
 
+      // 音乐播放动画
       var musicBars = document.getElementsByClassName("bars");
       Array.from(musicBars).forEach(element => {
         element.classList.remove("now");
       });
       elm.classList.add("now");
 
-      var source = document.getElementById('audioSource');
-      source.src = "https://typora-1259024198.cos.ap-beijing.myqcloud.com/" + elm.getAttribute('data-value');
+      // 更换源
+      player.src = "https://typora-1259024198.cos.ap-beijing.myqcloud.com/" + elm.getAttribute('data-value');
       player.load(); //call this to just preload the audio without playing
       player.play();
     });
   }
 
+  // 顺序播放一遍全部音乐
   const loopAll = document.getElementById('loop-all');
   const songs = document.getElementsByClassName('song');
+  const allSongsSources = [];
+  Array.from(songs).forEach(ele =>{
+    allSongsSources.push("https://typora-1259024198.cos.ap-beijing.myqcloud.com/" + ele.getAttribute('data-value'));
+  });
+  const totalSongs = allSongsSources.length;
+  var accumulator = totalSongs;
   loopAll.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
+    barsDancing(0);
+
+    player.loop = false;//禁止循环，否则无法触发ended事件 
+    player.src = allSongsSources[0];         //每次读数组最后一个元素 
+    player.addEventListener('ended', playEndedHandler, false); 
+    player.play(); 
   });
+
+  function playEndedHandler(){ 
+    const index = ++accumulator % totalSongs;
+    barsDancing(index);
+    player.src = allSongsSources[index]; 
+    player.play(); 
+    !allSongsSources.length && player.removeEventListener('ended',playEndedHandler,false);//只有一个元素时解除绑定 
+  }   
+
+  // 音乐播放动画
+  function barsDancing(index){
+    var audio = document.getElementsByClassName("audio");
+    Array.from(audio).forEach(element => {
+      element.classList.remove("now");
+    });
+    songs[index].classList.add("now");
+  }
 
 });
