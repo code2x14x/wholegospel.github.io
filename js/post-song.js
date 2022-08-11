ea2d0ee35286af8424cda375e7e45370e864353b
fengxinbao <fengxinbao@360.cn>
2022-08-11 19:25:11 +0800

    Add music
    Former-commit-id: dc9b0c41d653d0c4cfa5a5b7a48ab5c4c12815c8 [formerly c03245c3fc3112ea6cc2652329bb84908407b732]
    Former-commit-id: d8397d9053ec5f7bcd8a2bc41275781a15a19184

--- js/post-song.js
+++ js/post-song.js
@@ -7,27 +7,19 @@ $(document).ready(function() {
   const player = document.getElementById('song-player');
   const songTitles = document.getElementsByClassName('song-title');
 
-  // const songTitles = $('.song-title');
   for(const songTitle of songTitles) {
     songTitle.addEventListener('click', (e) => {
       e.preventDefault();
-      // if (player.paused) {
-        $("audio").each(function(){
-          if(!$(this)[0].paused){
-              $(this)[0].pause();
-          }
-        });
-        var elm = e.target;
-        // var audio = document.getElementById('audio');
-        var source = document.getElementById('audioSource');
-        source.src = "https://typora-1259024198.cos.ap-beijing.myqcloud.com/" + elm.getAttribute('data-value');
-        player.load(); //call this to just preload the audio without playing
-        player.play();
-        // btn.textContent = 'Play';
-      // } else {
-        // player.pause();
-        // btn.textContent = 'Pause';
-      // }
+      $("audio").each(function(){
+        if(!$(this)[0].paused){
+            $(this)[0].pause();
+        }
+      });
+      var elm = e.target;
+      var source = document.getElementById('audioSource');
+      source.src = "https://typora-1259024198.cos.ap-beijing.myqcloud.com/" + elm.getAttribute('data-value');
+      player.load(); //call this to just preload the audio without playing
+      player.play();
     });
 
   }
