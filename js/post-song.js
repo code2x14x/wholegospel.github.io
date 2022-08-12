83ca82966a2ca95c8c7b4411c34bdd0c6b13ac13
fengxinbao <fengxinbao@360.cn>
2022-08-12 16:53:26 +0800

    Add music : optimize
    Former-commit-id: fdaf70746095de484f9a6f5a6c865f35cb2c7406 [formerly 96992716b438073763c59540c966d668284e4b3d]
    Former-commit-id: 7d2503f96be612fad904f294da3deeb4ce58062d

--- js/post-song.js
+++ js/post-song.js
@@ -40,9 +40,16 @@ $(document).ready(function () {
     );
   });
   const totalSongs = allSongsSources.length;
-  var accumulator = totalSongs;
+  let accumulator = totalSongs;
+  let loopAllClickCnt = 2;
   loopAll.addEventListener('click', (e) => {
     e.preventDefault();
+    if (++loopAllClickCnt % 2 == 0) {
+      document.getElementById('loop-all-indicator').innerText = '';
+      player.loop = true; //禁止循环，否则无法触发ended事件
+      player.removeEventListener('ended', playEndedHandler, false);
+      return;
+    }
     document.getElementById('loop-all-indicator').innerText = '全部循环...';
 
     barsDancing(0);
@@ -75,55 +82,60 @@ $(document).ready(function () {
   // 倒计时
   let countDownDate = 0;
   let timer = 0;
-  const countDowns = document.getElementsByClassName('count-down');
-  let durationLast = 0;
-  let durationClickCnt = 2;
-  Array.from(countDowns).forEach((ele) => {
-    ele.addEventListener('click', (event) => {
-      let duration = ele.getAttribute('data-value');
-      console.log(duration);
+  const countDown = document.getElementById('count-down');
+  const cancleCountDown = document.getElementById('cancle-count-down');
+  let durationClickCnt = 5;
+  cancleCountDown.addEventListener('click', (event) => {
+    event.preventDefault();
+    clearInterval(timer);
+    document.getElementById('count-down-timer').innerText = '';
+  })
+  countDown.addEventListener('click', (event) => {
+    event.preventDefault();
+    durationClickCnt++;
+    clearInterval(timer);
+    let duration = 0;
+    if (durationClickCnt % 5 == 0) {
+      document.getElementById('count-down-timer').innerText = '';
+      return;
+    } else if (durationClickCnt % 5 == 1) {
+      duration = 10;
+    } else if (durationClickCnt % 5 == 2) {
+      duration = 15;
+    } else if (durationClickCnt % 5 == 3) {
+      duration = 20;
+    } else if (durationClickCnt % 5 == 4) {
+      duration = 30;
+    }
+    console.log(duration);
+    countDownDate = new Date().getTime() + duration * 60 * 1000;
+    // find the interval between now and the countdown time
+    let timeLeft = duration * 60 * 1000;
+    // time calculations for days, hours, minutes and seconds
+    const countDownTimer = document.getElementById('count-down-timer');
+    countDownTimer.innerText = duration + ':00';
+    // countdown timer
+    timer = setInterval(function () {
+      // get today's date and time in milliseconds
+      let now = new Date().getTime();
 
-      clearInterval(timer);
-      durationClickCnt++;
-      // 相邻两次点击同一个按钮时，就是开关
-      if(duration == durationLast && durationClickCnt % 2 == 0) {
-        // 1 开 2 关 3 开 4 关
-        document.getElementById('count-down-timer').innerText = '';
-        return;
-      } else {
-        durationLast = duration;
-      }
-
-      countDownDate = new Date().getTime() + duration * 60 * 1000;
       // find the interval between now and the countdown time
-      let timeLeft = duration * 60 * 1000;
+      timeLeft = countDownDate - now;
+
       // time calculations for days, hours, minutes and seconds
-      const countDownTimer = document.getElementById('count-down-timer');
-      countDownTimer.innerText = duration + ':00';
-      // countdown timer
-      timer = setInterval(function () {
-        // get today's date and time in milliseconds
-        let now = new Date().getTime();
+      const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+      const seconds = Math.floor((timeLeft / 1000) % 60);
 
-        // find the interval between now and the countdown time
-        timeLeft = countDownDate - now;
-
-        // time calculations for days, hours, minutes and seconds
-        const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
-        const seconds = Math.floor((timeLeft / 1000) % 60);
-
-        // const countDownTimer = document.getElementById('count-down-timer');
-        const minutesStr = minutes < 10 ? "0" + minutes : minutes;
-        const secondsStr = seconds < 10 ? "0" + seconds : seconds;
-        countDownTimer.innerText = minutesStr + ':' + secondsStr;
-        // clearing countdown when complete
-        if (timeLeft < 0) {
-          clearInterval(timer);
-          countDownTimer.innerText = '';
-          player.pause();
-        }
-      }, 1000);
-    });
+      // const countDownTimer = document.getElementById('count-down-timer');
+      const minutesStr = minutes < 10 ? '0' + minutes : minutes;
+      const secondsStr = seconds < 10 ? '0' + seconds : seconds;
+      countDownTimer.innerText = minutesStr + ':' + secondsStr;
+      // clearing countdown when complete
+      if (timeLeft < 0) {
+        clearInterval(timer);
+        countDownTimer.innerText = '';
+        player.pause();
+      }
+    }, 1000);
   });
-
 });
