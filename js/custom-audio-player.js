666001709f7445db859ff5b08fa484f3c3749859
fengxinbao <fengxinbao@360.cn>
2022-10-07 22:54:06 +0800

    新功能：歌词同步：bug fix
    Former-commit-id: b0b894b2e32619637e9409eaf61b867dfbd19604 [formerly 1c03f4a82f5587aaf9d2268222415d5139350e4e]
    Former-commit-id: f91e985663d565543848e59eb38e56a27f017a71

--- js/custom-audio-player.js
+++ js/custom-audio-player.js
@@ -287,27 +287,25 @@ $(function () {
 
   function play(audioElem){
     var current = audioTrackElem.currentTime;
-    if(current == 0) {
+    if(current == 0 && audioElem.dataset.lyrics) {
+      // 更改标题配图
+      const imgSrc = audioElem.dataset.image;
+      if(imgSrc) postHeadElem.style.backgroundImage =  "url('" + osUrl + "wg-img/hymn/" + imgSrc + "')";
+      else postHeadElem.style.backgroundImage =  "url('" + osUrl + defultImg + "')";
 
-    // 更改标题配图
-    const imgSrc = audioElem.dataset.image;
-    console.log("imgSrc = " + imgSrc)
-    if(imgSrc) postHeadElem.style.backgroundImage =  "url('" + osUrl + imgSrc + "')";
-    else postHeadElem.style.backgroundImage =  "url('" + osUrl + defultImg + "')";
-
-    const rl = document.getElementById("lyrics-1");
-    rl.innerHTML = '';
-    const ly = document.createTextNode(audioElem.dataset.lyrics);
-    rl.append(ly);
-    if(audioElem.dataset.lyrics) document.querySelector(".post-head-bg-img").classList.add("active");
-    else document.querySelector(".post-head-bg-img").classList.remove("active");
-    rabbitLyrics = new RabbitLyrics({
-      element: document.getElementById('lyrics-1'),
-      height: 100,
-      mediaElement: document.getElementById('audio-track')
-    });
-    
+      const rl = document.getElementById("lyrics-1");
+      rl.innerHTML = '';
+      const ly = document.createTextNode(audioElem.dataset.lyrics);
+      rl.append(ly);
+      if(audioElem.dataset.lyrics) document.querySelector(".post-head-bg-img").classList.add("active");
+      else document.querySelector(".post-head-bg-img").classList.remove("active");
+      rabbitLyrics = new RabbitLyrics({
+        element: document.getElementById('lyrics-1'),
+        height: 100,
+        mediaElement: document.getElementById('audio-track')
+      });
     }
+
     barsDancing(audioElem);
     playButton.hide();
     pauseButton.fadeIn();
