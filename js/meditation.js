9b8f4311c00900d8be1b84a39166cd1d401ccd4d
fengxinbao <fengxinbao@360.cn>
2024-01-12 13:55:38 +0800

    feat: Meditation

--- js/meditation.js
+++ js/meditation.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-function showMeditation() {
-  document.getElementById("meditation").style.display = "flex";
-}
-function hideMeditation() {
-  document.getElementById("meditation").style.display = "none";
-}
+  function showMeditation() {
+    document.getElementById("meditation").style.display = "flex";
+  }
+  function hideMeditation() {
+    document.getElementById("meditation").style.display = "none";
+  }
 
-$("#meditation_img").click(function () {
-    $("div#meditation").fadeIn('500');
-    $('body').css('overflow', 'hidden');  //ADD THIS
-});
+  $("#meditation_img").click(function () {
+    $("div#meditation").fadeIn("500");
+    $("body").css("overflow", "hidden"); //ADD THIS
+  });
 
-$(document).on('click', '.meditation_card_closeBtn', function () {
-    $("div#meditation").fadeOut('500');
-    $('body').css('overflow', 'auto');  //ADD THIS
-});
+  $(document).on("click", "#meditation_card_closeBtn", function () {
+    $("div#meditation").fadeOut("500");
+    $("body").css("overflow", "auto"); //ADD THIS
+  });
 });
