82356ea0dc1d38c7b0848650e80272a72c1d2d56
fengxinbao <fengxinbao@360.cn>
2024-01-12 14:12:23 +0800

    feat: Meditation

--- js/meditation.js
+++ js/meditation.js
@@ -8,11 +8,13 @@ $(document).ready(function () {
 
   $("#meditation_img").click(function () {
     $("div#meditation").fadeIn("500");
+    document.getElementById("meditation").style.display = "flex";
     $("body").css("overflow", "hidden"); //ADD THIS
   });
 
   $(document).on("click", "#meditation_card_closeBtn", function () {
     $("div#meditation").fadeOut("500");
+    document.getElementById("meditation").style.display = "none";
     $("body").css("overflow", "auto"); //ADD THIS
   });
 });
