d2da1103028f20e9fc8ee8d41aa806af986f8faa
fengxinbao <fengxinbao@360.cn>
2024-01-13 01:17:53 +0800

    Update meditation.js

--- js/meditation.js
+++ js/meditation.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-  function showMeditation() {
-    document.getElementById("meditation").style.display = "flex";
-  }
-  function hideMeditation() {
-    document.getElementById("meditation").style.display = "none";
-  }
-
   $("#meditation_img").click(function () {
     $("div#meditation").fadeIn("300");
     document.getElementById("meditation").style.display = "flex";
@@ -14,7 +7,7 @@ $(document).ready(function () {
 
   $(document).on("click", "#meditation_card_closeBtn", function () {
     $("div#meditation").fadeOut("500");
-    document.getElementById("meditation").style.display = "none";
+    // document.getElementById("meditation").style.display = "none";
     $("body").css("overflow", "auto"); //ADD THIS
   });
 
